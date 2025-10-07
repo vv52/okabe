74012c59fb7b430f710f036c2490b8b3f83310bb
Vanja Venezia <vanjavenezia@gmail.com>
2025-10-07 00:09:33 -0700

    implement file interpretation, fix bug in quote handling causing OOB error when using a multiline token buffer, add logical & | x| !, add preprocessor to strip comments from input file before passing back to interpreter, add several more examples and ensure all run as intended in interpreter, add debug mode for interpreter, name language

--- okabe.nim
+++ okabe.nim
@@ -59,6 +59,8 @@ proc rpush() : void
 docs[">r"] = "( a -- ) r( -- a )"
 proc rpop() : void
 docs["r>"] = "( -- a ) r( a -- )"
+proc rcmp : void
+docs["rcmp"] = "( a -- a t ) r( a -- a ) checks if top of stack and rstack are the same, returns 1 if true and 0 if false"
 proc qpush() : void
 docs[">q"] = "$( s -- ) q( -- q ) pop $stack and push to qstack"
 proc qpop() : void
@@ -86,17 +88,37 @@ proc lt0() : void
 docs["lt0"] = ("( a -- a b ) if a is less than 0, b is 1, otherwise b is 0")
 proc eq0() : void
 docs["eq0"] = ("( a -- a b ) if a is equal to 0, b is 1, otherwise b is 0")
+proc land() : void
+docs["&"] = ("( t f -- f ) logical and, only returns 1 if both are also 1, otherwise returns 0")
+proc lor() : void
+docs["|"] = ("( t f -- t ) logical or, returns 1 if either is 1, only returns 0 if both are 0")
+proc lxor() : void
+docs["x|"] = ("( t f -- t ) logical xor, returns 0 unless top stack contains a 1 and a 0, in which case return 1")
+proc eq() : void
+docs["="] = ("( a b -- a c ) if a is equal to b, c is 1, otherwise c is 0")
+proc linv() : void
+docs["!"] = ("( a -- b ) if a is 1, b is 0; if a is 0, b is 1")
 proc word() : void
 docs["proc"] = "$( s -- ) ()( q -- ) make new word from top of string stack that does top of qstack"
 proc stackDump(s : Stack) : void
+proc memDump() : void
 proc executeQuote(q : string) : void
 proc parseToken(token : string) : void
 proc repl() : void
+proc preprocess(file : string) : string
 proc interpret(file : string) : void
 proc compile(file : string) : void
 
+# let BINARY = "$b"
+# let DECIMAL = "$i"
+# let HEXADECIMAL = "$h"
+# let FLOAT = "$f"
+# let NUMTYPE = [ DECIMAL, BINARY, HEXADECIMAL, FLOAT ]
+let base = [ "$i", "$b", "$h", "$f" ]
+
 var debug = false
 var help_mode = false
+var interpreting = false
 
 var stack = newStack[int](capacity = 16)     # THE stack
 var rstack = newStack[int](capacity = 16)    # rstack for forth algs
@@ -105,6 +127,9 @@ var qstack = newStack[string](capacity = 16) # stack for quoted code in conditio
 
 var dreg : string
 var buffer : seq[string]
+var ifbuffer : string
+var iftokens : seq[string]
+# var ofbuffer : string
 
 var procs = newStringTable()
 
@@ -120,18 +145,21 @@ proc dump =
     echo stack.pop()
   except:
     echo: "ERROR: nothing on stack to pop"
+    memdump()
 
 proc sdump =
   try:
     echo sstack.pop()
   except:
     echo: "ERROR: nothing on $stack to pop"
+    memdump()
 
 proc top =
   try:
     echo stack.peek()
   except:
     echo: "ERROR: nothing on stack to peek"
+    memdump()
 
 proc add =
   try:
@@ -140,6 +168,7 @@ proc add =
     stack.push(a + b)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
   
 proc sub =
   try:
@@ -148,6 +177,7 @@ proc sub =
     stack.push(b - a)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
   
 proc mul =
   try:
@@ -156,6 +186,7 @@ proc mul =
     stack.push(a * b)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
 
 proc divide = 
   try:
@@ -165,8 +196,10 @@ proc divide =
     stack.push(x[0])
   except DivByZeroDefect:
     echo "ERROR: division by zero"
+    memdump()
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
   
 proc divrem = 
   try:
@@ -177,8 +210,10 @@ proc divrem =
     stack.push(x[0])
   except DivByZeroDefect:
     echo "ERROR: division by zero"
+    memdump()
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
   
 proc modulus = 
   try:
@@ -190,18 +225,21 @@ proc modulus =
     echo "ERROR: division by zero"
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
 
 proc inc =
   try:
     stack.push(stack.pop + 1)
   except:
     echo "ERROR: nothing on stack to increment"
+    memdump()
 
 proc dec =
   try:
     stack.push(stack.pop - 1)
   except:
     echo "ERROR: nothing on stack to decrement"
+    memdump()
 
 proc bsl =
   try:
@@ -210,6 +248,7 @@ proc bsl =
     stack.push(x shl times)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
 
 proc bsr =
   try:
@@ -218,18 +257,21 @@ proc bsr =
     stack.push(x shr times)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
 
 proc dup =
   try:
     stack.push(stack.peek())
   except:
     echo "ERROR: nothing on stack to duplicate"
+    memdump()
   
 proc drop =
   try:
     discard stack.pop()
   except:
     echo "ERROR: nothing on stack to drop"
+    memdump()
 
 proc rot =
   try:
@@ -241,6 +283,7 @@ proc rot =
     stack.push(c)
   except:
     echo "ERROR: less than three numbers on stack"
+    memdump()
 
 proc swap =
   try:
@@ -250,6 +293,7 @@ proc swap =
     stack.push(b)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
 
 proc over =
   try:
@@ -260,6 +304,7 @@ proc over =
     stack.push(b)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
 
 proc pick =
   try:
@@ -268,6 +313,7 @@ proc pick =
     stack.push(temp[i])
   except:
     echo "ERROR: nothing on stack"
+    memdump()
   
 proc tuck =
   try:
@@ -276,6 +322,7 @@ proc tuck =
     stack.push(top)
   except:
     echo "ERROR: less than two numbers on stack"
+    memdump()
   
 proc roll =
   try:
@@ -295,18 +342,21 @@ proc roll =
       stack.push(newTop)
   except:
     echo "ERROR: nothing on stack"
+    memdump()
 
 proc sdup =
   try:
     sstack.push(sstack.peek())
   except:
     echo "ERROR: nothing on $stack"
+    memdump()
   
 proc sdrop =
   try:
     discard sstack.pop
   except:
     echo "ERROR: nothing on $stack"
+    memdump()
 
 proc sswap =
   try:
@@ -316,24 +366,37 @@ proc sswap =
     sstack.push(s2)
   except:
     echo "ERROR: less than two strings on $stack"
+    memdump()
 
 proc stop =
   try:
     echo sstack.peek
   except:
     echo "ERROR: nothing on $stack to peek"
+    memdump()
 
 proc rpush =
   try:
     rstack.push(stack.pop)
   except:
-    echo: "ERROR: nothing on stack to pop"
+    echo "ERROR: nothing on stack to pop"
+    memdump()
   
 proc rpop =
   try:
     stack.push(rstack.pop)
   except:
-    echo: "ERROR: nothing on rstack to pop"
+    echo "ERROR: nothing on rstack to pop"
+    memdump()
+
+proc rcmp =
+  try:
+    if stack.peek == rstack.peek:
+      stack.push(1)
+    else: stack.push(0)
+  except:
+    echo "ERROR: cannot compare stack and rstack if either is empty"
+    memdump()
   
 proc quote =
   token_ptr += 1
@@ -379,30 +442,35 @@ proc qpop() =
     sstack.push(qstack.pop)
   except:
     echo "ERROR: nothing on qstack to pop"
+    memdump()
 
 proc exqtop =
   if not qstack.isEmpty:
     executeQuote(qstack.peek)
   else:
     echo "ERROR: nothing on qstack to peek"
+    memdump()
 
 proc exqpop =
   if not qstack.isEmpty:
     executeQuote(qstack.pop)
   else:
     echo "ERROR: nothing on qstack to pop"
+    memdump()
 
 proc exc =
   if not qstack.isEmpty:
     discard qstack.pop
   else:
     echo "ERROR: nothing on qstack to drop"
+    memdump()
 
 proc deferex =
   if not qstack.isEmpty:
     dreg = qstack.pop
   else:
     echo "ERROR: nothing on qstack to pop"
+    memdump()
 
 proc dex =
   executeQuote(dreg)
@@ -412,10 +480,12 @@ proc question =
     discard stack.peek
   except:
     echo "ERROR: no condition result on stack to pop"
+    memdump()
   try:
     discard qstack.peek
   except:
     echo "ERROR: no branch on qstack to pop"
+    memdump()
   try:
     if stack.pop > 0:
       executeQuote(qstack.pop)
@@ -430,8 +500,10 @@ proc questionelse =
     discard stack.peek
   except:
     echo "ERROR: no condition result on stack to pop"
+    memdump()
   if qstack.len < 2:
     echo "ERROR: less than two quotes on qstack"
+    memdump()
   try:
     if stack.pop > 0:
       discard qstack.pop
@@ -450,6 +522,7 @@ proc gt0 =
     else: stack.push(0)
   except:
     echo "ERROR: nothing on stack to compare"
+    memdump()
 
 proc lt0 =
   try:
@@ -458,6 +531,7 @@ proc lt0 =
     else: stack.push(0)
   except:
     echo "ERROR: nothing on stack to compare"
+    memdump()
 
 proc eq0 =
   try:
@@ -466,22 +540,72 @@ proc eq0 =
     else: stack.push(0)
   except:
     echo "ERROR: nothing on stack to compare"
+    memdump()
+
+proc land() =
+  try:
+    if stack.pop + stack.pop == 2:
+      stack.push(1)
+    else: stack.push(0)
+  except:
+    echo "ERROR: less than two values on stack"
+    memdump()
+  
+proc lor() =
+  try:
+    let t = stack.pop + stack.pop
+    if t == 1 or t == 2:
+      stack.push(1)
+    else: stack.push(0)
+  except:
+    echo "ERROR: less than two values on stack"
+    memdump()
+  
+proc lxor() =
+  try:
+    if stack.pop + stack.pop == 1:
+      stack.push(1)
+    else: stack.push(0)
+  except:
+    echo "ERROR: less than two values on stack"
+    memdump()
+
+proc eq =
+  try:
+    if stack.pop - stack.peek == 0:
+      stack.push(1)  
+    else: stack.push(0)
+  except:
+    echo "ERROR: less than two values on stack"
+    memdump()
+
+proc linv =
+  try:
+    if stack.pop == 0:
+      stack.push(1)  
+    else: stack.push(0)
+  except:
+    echo "ERROR: nothing on stack"
+    memdump()
 
 proc word =
   try:
     discard sstack.peek
   except:
     echo "ERROR: no proc name provided; nothing on $stack"
+    memdump()
   try:
     discard qstack.peek
   except:
     echo "ERROR: cannot declare proc without quote body; nothing on qstack"
+    memdump()
   try:
     let name = sstack.pop.strip
     procs[name] = qstack.pop
     echo fmt"""{name} created"""
   except:
     echo "ERROR: could not create proc"
+    memdump()
 
 proc stackDump(s : Stack) =
   var i : int = 0
@@ -491,6 +615,24 @@ proc stackDump(s : Stack) =
     echo fmt"  {i}  |   {$item}"
     i += 1
 
+proc memDump =  
+  if not stack.isEmpty:
+    echo "\nSTACK"
+    stackDump(stack)
+  if not rstack.isEmpty:
+    echo "\nRSTACK"
+    stackDump(rstack)
+  if not sstack.isEmpty:
+    echo "\n$STACK"
+    stackDump(sstack)
+  if not qstack.isEmpty:
+    echo "\nQSTACK"
+    stackDump(qstack)
+  if dreg != "":
+    echo "DREG"
+    echo dreg
+  echo ""
+
 proc executeQuote(q : string) =
   let return_ptr = token_ptr
   let tokens : seq[string] = q.split(parseRule)
@@ -499,9 +641,14 @@ proc executeQuote(q : string) =
   while token_ptr < tokens.len:
     let token = tokens[token_ptr]
     if token.len > 0:
+      if debug:
+        echo fmt"""({token_ptr}) {token}"""
+        # memDump()
       parseToken(token)
     token_ptr += 1
   token_ptr = return_ptr
+  if interpreting:
+    buffer = iftokens
 
 proc parseToken(token : string) =
   var x : int
@@ -540,6 +687,7 @@ proc parseToken(token : string) =
         of "$top": stop()
         of ">r": rpush()
         of "r>": rpop()
+        of "rcmp": rcmp()
         of ">q": qpush()
         of "q>": qpop()
         of "(": quote()
@@ -553,8 +701,17 @@ proc parseToken(token : string) =
         of "gt0": gt0()
         of "lt0": lt0()
         of "eq0": eq0()
+        of "&": land()
+        of "|": lor()
+        of "x|": lxor()
+        of "=": eq()
+        of "!": linv()
         of "proc": word()
         of "help": help()
+        of "bin": echo "TODO: swap base to BINARY"
+        of "hex": echo "TODO: swap base to HEXADECIMAL"
+        of "f": echo "TODO: swap num mode to FLOAT"
+        of "dec": echo "TODO: swap base to DECIMAL"
         else:
           if token in procs:
             executeQuote(procs[token])
@@ -576,31 +733,49 @@ proc repl =
               echo fmt"""No help available for "{token}""""
             else: echo docs[token]
             help_mode = false
-          else: parseToken(token)
+          else: 
+            if debug:
+              echo fmt"""[{token_ptr}] {token}"""
+            parseToken(token)
         token_ptr += 1
       echo "ok"
       if debug:
-        if not stack.isEmpty:
-          echo "\nSTACK"
-          stackDump(stack)
-        if not rstack.isEmpty:
-          echo "\nRSTACK"
-          stackDump(rstack)
-        if not sstack.isEmpty:
-          echo "\n$STACK"
-          stackDump(sstack)
-        if not qstack.isEmpty:
-          echo "\nQSTACK"
-          stackDump(qstack)
-        if dreg != "":
-          echo "DREG"
-          echo dreg
-        echo ""
+        memDump()
     else:
       should_end = true
 
+proc preprocess(file : string) : string = 
+  var processed = """"""
+  var line = """"""
+  let raw = open(file)
+  while readLine(raw, line):
+    var i = 0
+    while i < line.len:
+      if line[i] == '#':
+        i = line.len
+      else:
+        processed.add(line[i])
+        i += 1
+    processed.add(' ')
+  return processed
+
 proc interpret(file : string) =
-  echo "TODO: add file interpreter"
+  interpreting = true
+  ifbuffer = preprocess(file)
+  iftokens = ifbuffer.split(parseRule)
+  let tokens : seq[string] = iftokens
+  buffer = iftokens
+  token_ptr = 0
+  while token_ptr < tokens.len:
+    let token = tokens[token_ptr]
+    if token.len > 0:
+      if help_mode:
+        help_mode = false
+      else: 
+        parseToken(token)
+        if debug:
+          memDump()
+    token_ptr += 1
 
 proc compile(file : string) =
   echo "TODO: add file compiler to asm or nim"
@@ -616,5 +791,10 @@ if isMainModule:
       repl()
     else:
       interpret(params[0])
-  else:
-    echo "TODO: File handling and options"
+  elif params.len == 2:
+    case params[0]:
+    of "debug":
+      debug = true
+      interpret(params[1])
+    else:
+      echo fmt"Unknown command: {params[0]}"
